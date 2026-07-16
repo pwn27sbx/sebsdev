1a212ca954c1576f2a095aa2d3e5999858a663f8
pwn27sbx <pwn27sbx@gmail.com>
2026-07-15 23:51:02 -0500

    Add Tetris image in WEBP format to the public assets

--- src/Archive.jsx
+++ src/Archive.jsx
@@ -3,11 +3,12 @@ import { motion, useSpring, useMotionValue, useScroll, useTransform } from 'fram
 import { Link } from 'react-router-dom';
 
 const projects = [
-  { id: "01", year: "2024", title: "NEXUS", category: "Frontend", img: "/img/Nexus.webp", link: "https://nexus-drab-one.vercel.app/" },
-  { id: "02", year: "2024", title: "SERVICIOS GENERALES", category: "Fullstack", video: "https://www.w3schools.com/html/mov_bbb.mp4", link: "https://roi-servicios.vercel.app/" },
-  { id: "03", year: "2024", title: "FUXION OPORTUNIDAD", category: "Landing Page", img: "/img/Fuxion.webp", link: "https://fuxionoportunidad.vercel.app/" },
-  { id: "04", year: "2023", title: "TRANSPORTES PREMIUM", category: "UI/UX", img: "/img/TransPremium.webp", link: "https://trasnportesjuan.vercel.app/" },
-  { id: "05", year: "2024", title: "GRUPO HIRBELL", category: "Corporate", img: "/img/Hirbell.webp", link: "https://grupohirbell.vercel.app/" },
+  { id: "01", year: "2024", title: "NEXUS", category: "Frontend", img: "/img/9.webp", link: "https://nexus-drab-one.vercel.app/" },
+//   { id: "02", year: "2024", title: "SERVICIOS GENERALES", category: "Fullstack", video: "https://www.w3schools.com/html/mov_bbb.mp4", link: "https://roi-servicios.vercel.app/" },
+  { id: "02", year: "2024", title: "SERVICIOS GENERALES", category: "Landing Page",  img: "/img/4.webp", link: "https://roi-servicios.vercel.app/" },
+  { id: "03", year: "2024", title: "FUXION OPORTUNIDAD", category: "Landing Page", img: "/img/7.webp", link: "https://fuxionoportunidad.vercel.app/" },
+  { id: "04", year: "2023", title: "TRANSPORTES PREMIUM", category: "Landing Page", img: "/img/13.webp", link: "https://trasnportesjuan.vercel.app/" },
+  { id: "05", year: "2024", title: "GRUPO HIRBELL", category: "Landing Page", img: "/img/4.webp", link: "https://grupohirbell.vercel.app/" },
   { id: "06", year: "2023", title: "TETRIS ARCADE", category: "JavaScript / HTML", img: "/img/Tetris.webp", link: "/tetris/index.html" },
   { id: "07", year: "2022", title: "PAINT", category: "JavaScript / HTML", img: "/img/Paint.webp", link: "/paint/index.html" }
 ];
