a2960cb3d29820a42c6f77d7e46111c0349f9693
pwnsxb <pwn27sbx@gmail.com>
2026-08-01 16:52:15 -0500

    refactor: wrap animation style properties in object spread for Hero component styling

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -70,13 +70,13 @@ const Hero = () => {
               </GlitchText>
             </div>
             <motion.div
-              style={
-                {
-                  width: lineWidth,
+              style={{
+                width: lineWidth,
+                ...({
                   "--after-duration": "2.7s",
                   "--before-duration": "1.8s",
-                } as CustomCSSProperties
-              }
+                } as CustomCSSProperties)
+              }}
               className="glitch-box-anim h-[2vw] sm:h-[1.5vw] bg-transparent border-[2px] border-[#111] dark:border-[#a3a3a3] mx-2 sm:mx-4 transition-colors duration-300 hover:border-[#00A889] shrink-0 gpu"
             />
             <div
